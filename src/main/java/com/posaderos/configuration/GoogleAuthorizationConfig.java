d740a867e6f24031b5fced9f3258e72a071c24ef
matiesmengo <m.sureda@avoristravel.com>
2021-09-18 20:23:57 +0200

    heroku credential host

--- src/main/java/com/posaderos/configuration/GoogleAuthorizationConfig.java
+++ src/main/java/com/posaderos/configuration/GoogleAuthorizationConfig.java
@@ -1,18 +1,24 @@
 package com.posaderos.configuration;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class GoogleAuthorizationConfig {
@@ -22,11 +28,24 @@ public class GoogleAuthorizationConfig {
     private static final String CREDENTIALS_PATH = "/google-sheets-client-secret.json";
     private static final String TOKENS_PATH = "googleSpreadSheets.xls";
 
-    private Credential authorize() throws IOException {
+    private Credential authorize() throws IOException, GeneralSecurityException {
         final InputStream in = GoogleAuthorizationConfig.class.getResourceAsStream(CREDENTIALS_PATH);
+        final GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
-        return GoogleCredential.fromStream(in)
-                .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+        final List<String> scopes = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+
+        final GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, clientSecrets, scopes)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        final LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder()
+                .setHost("https://bot-posaderos.herokuapp.com")
+                .build();
+
+        return new AuthorizationCodeInstalledApp(flow, localServerReceiver)
+                .authorize("user");
     }
 
     public Sheets getSheetsService() throws IOException, GeneralSecurityException {
